d7b996ae9ca1bd34270ce3db6e4b7aa667e762ce
zekibaklan <zekibaklan@icloud.com>
2024-03-07 05:07:23 +0300

    FavoriteView done

---
+++ KaracaApp/MotionAnimationView.swift
@@ -0,0 +1,8 @@
+//
+//  MotionAnimationView.swift
+//  KaracaApp
+//
+//  Created by Zeki Baklan on 7.03.2024.
+//
+
+import Foundation
---
+++ KaracaApp/View/TabView/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  KaracaApp
+//
+//  Created by Zeki Baklan on 7.03.2024.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
