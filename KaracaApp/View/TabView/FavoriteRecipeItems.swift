a3301e3c2cf4d4d898becac2ae3345d803aa5049
zekibaklan <zekibaklan@icloud.com>
2024-03-14 01:35:19 +0300

    FavoriteRecipeView done

---
+++ KaracaApp/View/TabView/FavoriteRecipeItems.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteRecipeItems.swift
+//  KaracaApp
+//
+//  Created by Zeki Baklan on 13.03.2024.
+//
+
+import SwiftUI
+
+struct FavoriteRecipeItems: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteRecipeItems()
+}
--- KaracaApp/View/TabView/FavoriteRecipeView.swift
+++ KaracaApp/View/TabView/FavoriteRecipeView.swift
@@ -1,18 +1,18 @@
 //
-//  FavoriteView.swift
+//  FavoriteRecipeView.swift
 //  KaracaApp
 //
-//  Created by Zeki Baklan on 7.03.2024.
+//  Created by Zeki Baklan on 13.03.2024.
 //
 
 import SwiftUI
 
-struct FavoriteView: View {
+struct FavoriteRecipeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    FavoriteView()
+    FavoriteRecipeView()
 }
