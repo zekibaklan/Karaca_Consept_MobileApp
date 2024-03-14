c977eb86a8d7b3cfe65bd0dd165411d3ae23f3c8
zekibaklan <zekibaklan@icloud.com>
2024-03-15 01:19:26 +0300

    COMMENT VİEW compailing..

---
+++ KaracaApp/View/TabView/CommentView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentView.swift
+//  KaracaApp
+//
+//  Created by Zeki Baklan on 14.03.2024.
+//
+
+import SwiftUI
+
+struct CommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentView()
+}
