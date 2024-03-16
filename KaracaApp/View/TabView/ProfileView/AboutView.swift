e6410e5546abf6257a4b23dd922cced0dd471c26
zekibaklan <zekibaklan@icloud.com>
2024-03-16 23:51:59 +0300

    abouview done

--- KaracaApp/View/TabView/ProfileView/AboutView.swift
+++ KaracaApp/View/TabView/ProfileView/AboutView.swift
@@ -1,18 +1,18 @@
 //
-//  FavoriteRecipeView.swift
+//  AboutView.swift
 //  KaracaApp
 //
-//  Created by Zeki Baklan on 13.03.2024.
+//  Created by Zeki Baklan on 16.03.2024.
 //
 
 import SwiftUI
 
-struct FavoriteRecipeView: View {
+struct AboutView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    FavoriteRecipeView()
+    AboutView()
 }
