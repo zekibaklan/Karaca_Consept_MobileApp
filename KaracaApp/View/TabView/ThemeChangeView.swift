adead6924be230e8f193ed12f04ea7c71645878d
zekibaklan <zekibaklan@icloud.com>
2024-03-20 06:33:01 +0300

    theme change done

---
+++ KaracaApp/View/TabView/ThemeChangeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeChangeView.swift
+//  KaracaApp
+//
+//  Created by Zeki Baklan on 20.03.2024.
+//
+
+import SwiftUI
+
+struct ThemeChangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeChangeView()
+}
